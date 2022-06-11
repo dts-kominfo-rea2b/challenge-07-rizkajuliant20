2c62f8db1444b49d67650dd2b0fa0e3e4c8a1bb3
rizkajuliant20 <rizkariz20@gmail.com>
2022-06-12 02:29:29 +0700

    first commit for challenge 7

--- promise.js
+++ promise.js
@@ -1,8 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(result) => {
+    const resultIXX = await promiseTheaterIXX()
+    const resultVGC = await promiseTheaterVGC()
+    let marah = 0,
+        tidakMarah = 0
+    for (let i = 0; i < resultIXX.length; i++) {
+        if (resultIXX[i].hasil === 'marah') {
+            marah++
+        } else {
+            tidakMarah++
+        }
+    }
+
+    for (let i = 0; i < resultVGC.length; i++) {
+        if (resultVGC[i].hasil === 'marah') {
+            marah++
+        } else {
+            tidakMarah++
+        }
+    }
+
+    if (result === 'marah') return marah
+    return tidakMarah
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
